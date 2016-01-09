eb72686028bb2d393c7ff602e89fb611a6d728b0
Sarah Doczy <sarahdoczy@gmail.com>
2016-01-09 10:42:06 -0800

    initial commit

---
+++ js/realtor.js
@@ -0,0 +1,44 @@
+$(function(){
+    $('.submit-contact-form').submit(function(e){
+        if ($.trim($("#email").val()) === "" || $.trim($("#name").val()) === "" || $.trim($("#message").val()) === "" ) {
+            if ($.trim($("#email").val()) === "" ) {
+                $(".email.form-error").show();
+            }
+            if ($.trim($("#name").val()) === "" ) {
+                $(".name.form-error").show();
+            }
+            if ($.trim($("#message").val()) === "" ) {
+                $(".message.form-error").show();
+            }
+            return false;
+        }
+        e.preventDefault();
+        $.ajax({
+            url:'http://emilyschmidtrealtor.com/php/index.php',
+            type:'post',
+            data:$('.submit-contact-form').serialize(),
+            success:function(){
+                $("#messageSent").show();
+            }
+        });
+    });
+
+    function checkWindowSize() {
+        if ($(window).width() <= 767){  
+            $("#navbar").addClass('mobile-nav')
+        }
+        if ($(window).width() > 767){  
+            $("#navbar").removeClass('mobile-nav')
+        }    
+    }
+    $(window).resize(function(){
+        checkWindowSize();
+    });
+    checkWindowSize();
+    $(function () {
+        $('.navbar-collapse ul li a:not(.dropdown-toggle)').bind('click touchstart', function () {
+                $('.navbar-toggle:visible').click();
+        });
+    }); 
+});
+
